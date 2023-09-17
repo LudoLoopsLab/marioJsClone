ed69e23d411355349e888dd3c62f213812a68303
Ludovic Dumas <ludoloops@pm.me>
2023-09-17 18:40:24 -0400

    #2 Gravity (#12) close #2

--- index.js
+++ index.js
@@ -25,7 +25,10 @@ class Player {
   }
   update() {
     this.draw()
+    this.position.x += this.velocity.x
     this.position.y += this.velocity.y
+
+
     if (this.position.y + this.height + this.velocity.y <= canvas.height) {
       this.velocity.y += gravity
     } else this.velocity.y = 0
@@ -36,16 +39,87 @@ class Player {
 
 
 const player = new Player()
-player.update()
+
+const keys = {
+  right: {
+    pressed: false
+  },
+  left: {
+    pressed: false
+  },
+}
+
 
 
 const animate = () => {
   requestAnimationFrame(animate)
   c.clearRect(0, 0, canvas.width, canvas.height)
   player.update()
+
+  if (keys.right.pressed) {
+    player.velocity.x = 5
+  } else if (keys.left.pressed) {
+    player.velocity.x = -5
+  } else player.velocity.x = 0
+
+
+
+
 }
+
+
 
 
 animate()
 
 
+addEventListener('keydown', ({ code }) => {
+
+  switch (code) {
+    case 'KeyA':
+    case 'ArrowLeft':
+      keys.left.pressed = true
+      break
+
+    case 'KeyD':
+    case 'ArrowRight':
+      keys.right.pressed = true
+      break
+
+    case 'KeyW':
+    case 'ArrowUp':
+      player.velocity.y -= 20
+      break
+    case 'KeyS':
+    case 'ArrowDown':
+      console.log('down')
+      break
+  }
+})
+
+
+addEventListener('keyup', ({ code }) => {
+
+  switch (code) {
+    case 'KeyA':
+    case 'ArrowLeft':
+      keys.left.pressed = false
+      break
+
+
+    case 'KeyD':
+    case 'ArrowRight':
+      keys.right.pressed = false
+      break
+
+
+    case 'KeyW':
+    case 'ArrowUp':
+      player.velocity.y -= 20
+      break
+    case 'KeyS':
+    case 'ArrowDown':
+      console.log('down')
+      break
+  }
+})
