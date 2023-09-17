4c4b54236186588a6b4bf9c50a701226c2cf5e70
Ludo Loops <56033812+neuroloops@users.noreply.github.com>
2023-09-17 17:27:59 -0400

    #1-Player Creation (#11)

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
 const canvas = document.querySelector('canvas')
 const c = canvas.getContext('2d')
+canvas.width = innerWidth // full width
+canvas.height = innerHeight // full height
+class Player {
+  constructor() {
+    this.position = {
+      x: 100,
+      y: 100
+    }
+    this.width = 30
+    this.height = 30
 
-console.log(c)
+  }
+  draw() {
+    c.fillStyle = 'red'
+    c.fillRect(this.position.x, this.position.y, this.width, this.height)
+  }
+
+}
+
+
+const player = new Player()
+
+player.draw()
+
+
+
+
+
